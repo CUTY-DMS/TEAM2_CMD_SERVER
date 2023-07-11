fbc1cfc90e2a6f21531454e53f8ac87e99a040dc
JungHyun <b57062562@gmail.com>
2023-07-11 11:58:16 +0900

    ♻️ ::controller 수정

--- src/main/java/com/example/cmdproject_team2/domain/user/presentation/UserController.java
+++ src/main/java/com/example/cmdproject_team2/domain/user/presentation/UserController.java
@@ -7,6 +7,7 @@ import com.example.cmdproject_team2.domain.user.presentation.dto.response.AdminD
 import com.example.cmdproject_team2.domain.user.presentation.dto.response.StudentDetailsResponse;
 import com.example.cmdproject_team2.domain.user.presentation.dto.response.StudentListResponse;
 import com.example.cmdproject_team2.domain.user.presentation.dto.response.TokenResponse;
+import com.example.cmdproject_team2.domain.user.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,35 +16,40 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+    private final SignupAdminService signupAdminService;
+    private final LoginAdminService loginAdminService;
+    private final GetAdminDetailsService getAdminDetailsService;
+    private final GetStudentDetailsService getStudentDetailsService;
+    private final GetStudentListService getStudentListService;
+    private final ModifyAdminInfoService modifyAdminInfoService;
 
     @PostMapping("/admin/signup")
     public void signup(@RequestBody AdminSignupRequest request) {
-        userService.signup(request);
+        signupAdminService.signup(request);
     }
 
     @PostMapping("/admin/login")
     public TokenResponse login(@RequestBody AdminLoginRequest request) {
-        return userService.login(request);
+        return loginAdminService.login(request);
     }
 
     @GetMapping("/admin/getAdminDetails")
     public AdminDetailsResponse getAdminDetails(@PathVariable long userId) {
-        return userService.getAdminDetails(userId);
+        return getAdminDetailsService.getAdminDetails(userId);
     }
 
     @GetMapping("/admin/getStudentDetails")
     public StudentDetailsResponse getStudentDetails() {
-        return userService.getStudentDetails();
+        return getStudentDetailsService.getStudentDetails();
     }
 
     @GetMapping("/admin/getStudentList")
     public StudentListResponse getStudentList(){
-        return userService.getStudentList();
+        return getStudentListService.getStudentList();
     }
 
     @PatchMapping
     public void modifyAdminInfo(@PathVariable long userId, @RequestBody AdminModifyRequest request){
-        userService.modifyAdminInfo(userId, request);
+        modifyAdminInfoService.modifyAdminInfo(userId, request);
     }
 }
