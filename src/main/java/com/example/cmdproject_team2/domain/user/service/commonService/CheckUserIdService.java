958c5a4d567654943fc0672402358a73fbe3472d
4mjeo <4mj.eo@dsm.hs.kr>
2023-07-18 11:18:14 +0900

    ♻️ :: 패키지 이름 변경

--- src/main/java/com/example/cmdproject_team2/domain/user/service/commonService/CheckUserIdService.java
+++ src/main/java/com/example/cmdproject_team2/domain/user/service/commonService/CheckUserIdService.java
@@ -1,9 +1,8 @@
-package com.example.cmdproject_team2.domain.user.service.publicService;
+package com.example.cmdproject_team2.domain.user.service.commonService;
 
 import com.example.cmdproject_team2.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
