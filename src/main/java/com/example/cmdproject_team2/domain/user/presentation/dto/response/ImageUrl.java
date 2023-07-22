921ca918d36a2222266ebc88316e82e0684adfd3
4mjeo <4mj.eo@dsm.hs.kr>
2023-07-22 23:57:28 +0900

    🌧 :: 프로필 업로드 서비스

---
+++ src/main/java/com/example/cmdproject_team2/domain/user/presentation/dto/response/ImageUrl.java
@@ -0,0 +1,13 @@
+package com.example.cmdproject_team2.domain.user.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class ImageUrl {
+
+    
+}
