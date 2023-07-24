6a8c64809d836c9123c582c8003ad60fdc7b6efc
JungHyun <b57062562@gmail.com>
2023-07-24 23:17:56 +0900

    ♻️ ::exception 추가

--- src/main/java/com/example/cmdproject_team2/domain/notification/service/publicService/GetNotificationDetailsService.java
+++ src/main/java/com/example/cmdproject_team2/domain/notification/service/publicService/GetNotificationDetailsService.java
@@ -3,6 +3,7 @@ package com.example.cmdproject_team2.domain.notification.service.publicService;
 import com.example.cmdproject_team2.domain.notification.domain.Notification;
 import com.example.cmdproject_team2.domain.notification.domain.NotificationRepository;
 import com.example.cmdproject_team2.domain.notification.presentation.dto.response.NotificationResponse;
+import com.example.cmdproject_team2.global.exception.notification.NotificationNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +18,7 @@ public class GetNotificationDetailsService {
     public NotificationResponse getNotificationDetails(Long notificationId) {
 
         Notification notification = notificationRepository.findById(notificationId)
-                .orElseThrow();
+                .orElseThrow(()-> NotificationNotFoundException.EXCEPTION);
 
         return new NotificationResponse(notification);
     }
