11640440433c76815f5e43f5b3d589af227ae72d
4mjeo <4mj.eo@dsm.hs.kr>
2023-07-24 17:04:52 +0900

    🌱 :: exception 구조 변경

--- src/main/java/com/example/cmdproject_team2/global/exception/user/RefreshTokenNotFoundException.java
+++ src/main/java/com/example/cmdproject_team2/global/exception/user/RefreshTokenNotFoundException.java
@@ -1,4 +1,4 @@
-package com.example.cmdproject_team2.global.exception.token;
+package com.example.cmdproject_team2.global.exception.user;
 
 import com.example.cmdproject_team2.global.error.exception.BusinessException;
 import com.example.cmdproject_team2.global.error.exception.ErrorCode;
