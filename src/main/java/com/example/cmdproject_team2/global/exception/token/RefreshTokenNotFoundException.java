2f869fdf06920c6c9879df0dc94f9901eedf989e
4mjeo <4mj.eo@dsm.hs.kr>
2023-07-24 16:23:04 +0900

    ♻️ :: RefreshTokenNotFound 예외추가

---
+++ src/main/java/com/example/cmdproject_team2/global/exception/token/RefreshTokenNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.cmdproject_team2.global.exception.token;
+
+import com.example.cmdproject_team2.global.error.exception.BusinessException;
+import com.example.cmdproject_team2.global.error.exception.ErrorCode;
+
+public class RefreshTokenNotFoundException extends BusinessException {
+
+    public static final BusinessException EXCEPTION = new RefreshTokenNotFoundException();
+
+    public RefreshTokenNotFoundException() {
+        super(ErrorCode.REFRESH_TOKEN_NOT_FOUND);
+    }
+
+}
