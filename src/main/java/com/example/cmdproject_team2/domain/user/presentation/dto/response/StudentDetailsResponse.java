79eea242b866b8b19f47e71744afb65780619f6e
4mjeo <4mj.eo@dsm.hs.kr>
2023-07-27 22:21:10 +0900

    ♻️ :: 학생정보 response 수정

--- src/main/java/com/example/cmdproject_team2/domain/user/presentation/dto/response/StudentDetailsResponse.java
+++ src/main/java/com/example/cmdproject_team2/domain/user/presentation/dto/response/StudentDetailsResponse.java
@@ -26,11 +26,17 @@ public class StudentDetailsResponse {
     @Enumerated(EnumType.STRING)
     private MajorType majorType;
 
+    private String club;
+
+    private Date birth;
+
     public StudentDetailsResponse(User user) {
         username = user.getUsername();
         grader = user.getGrader();
         schoolClass = user.getSchoolClass();
         number = user.getNumber();
         majorType = user.getMajorType();
+        club = user.getClub();
+        birth = user.getBirth();
     }
 }
