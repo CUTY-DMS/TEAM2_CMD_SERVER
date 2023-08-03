ce72606427b556fe8aee68bca6f0076b395cc5a5
JungHyun <b57062562@gmail.com>
2023-08-03 10:16:44 +0900

    ♻️ :: 불필요한 메소드 삭제

--- src/main/java/com/example/cmdproject_team2/domain/notification/domain/NotificationRepository.java
+++ src/main/java/com/example/cmdproject_team2/domain/notification/domain/NotificationRepository.java
@@ -2,9 +2,5 @@ package com.example.cmdproject_team2.domain.notification.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
-
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    Optional<Notification> findByExpiredAt(LocalDateTime expiredAt);
 }
