fe59f4a2fedb2b5f6439c9887b1e35cf353bbc7a
JungHyun <b57062562@gmail.com>
2023-08-03 14:30:32 +0900

    ♻️ :: 공지 작성 코드 수정

--- src/main/java/com/example/cmdproject_team2/domain/notification/service/adminService/WriteNotificationService.java
+++ src/main/java/com/example/cmdproject_team2/domain/notification/service/adminService/WriteNotificationService.java
@@ -3,6 +3,8 @@ package com.example.cmdproject_team2.domain.notification.service.adminService;
 import com.example.cmdproject_team2.domain.notification.domain.Notification;
 import com.example.cmdproject_team2.domain.notification.domain.NotificationRepository;
 import com.example.cmdproject_team2.domain.notification.presentation.dto.request.NotificationRequest;
+import com.example.cmdproject_team2.domain.user.domain.User;
+import com.example.cmdproject_team2.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,11 +15,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class WriteNotificationService {
 
     private final NotificationRepository notificationRepository;
+    private final UserFacade userFacade;
 
     public void writeNotification(NotificationRequest request) {
 
+        User user = userFacade.currentUser();
+
         notificationRepository.save(
                 Notification.builder()
+                        .user(user)
                         .title(request.getTitle())
                         .content(request.getContent())
                         .expiredAt(request.getExpiredAt())
