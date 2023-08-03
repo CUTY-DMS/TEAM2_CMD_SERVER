8690f21d5cf63010a8a27e0f730d7ac17f4533f1
4mjeo <4mj.eo@dsm.hs.kr>
2023-08-03 15:22:10 +0900

    ♻️ :: 어노테이션 추가

--- src/main/java/com/example/cmdproject_team2/CmdProjectTeam2Application.java
+++ src/main/java/com/example/cmdproject_team2/CmdProjectTeam2Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
@@ -11,6 +12,7 @@ import java.util.TimeZone;
 @EnableRedisRepositories
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableWebSecurity
 public class CmdProjectTeam2Application {
 
     @PostConstruct
