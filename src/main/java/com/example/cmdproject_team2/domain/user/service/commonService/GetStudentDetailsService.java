ea217560266dea9df2ce6fc87943db0f0e434855
JungHyun <b57062562@gmail.com>
2023-08-06 15:53:07 +0900

    ♻️ ::

--- src/main/java/com/example/cmdproject_team2/domain/user/service/commonService/GetStudentDetailsService.java
+++ src/main/java/com/example/cmdproject_team2/domain/user/service/commonService/GetStudentDetailsService.java
@@ -1,10 +1,8 @@
 package com.example.cmdproject_team2.domain.user.service.commonService;
 
 import com.example.cmdproject_team2.domain.user.domain.User;
-import com.example.cmdproject_team2.domain.user.domain.UserRepository;
 import com.example.cmdproject_team2.domain.user.facade.UserFacade;
 import com.example.cmdproject_team2.domain.user.presentation.dto.response.StudentDetailsResponse;
-import com.example.cmdproject_team2.global.exception.user.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
