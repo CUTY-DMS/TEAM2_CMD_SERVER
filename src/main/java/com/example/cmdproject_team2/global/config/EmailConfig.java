a4c626062be7dd6afe8ae2a46a97cba132283dbb
4mjeo <4mj.eo@dsm.hs.kr>
2023-08-06 17:25:38 +0900

    ♻️ :: EmailConfig 파일 추가

---
+++ src/main/java/com/example/cmdproject_team2/global/config/EmailConfig.java
@@ -0,0 +1,27 @@
+package com.example.cmdproject_team2.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("4mj.eo@dsm.hs.kr");
+        mailSender.setPassword("Rlaaudwls1!");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+}
