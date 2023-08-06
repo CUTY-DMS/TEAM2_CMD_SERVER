7d95aa51ee39aba28600c3440936672995ae816c
4mjeo <4mj.eo@dsm.hs.kr>
2023-08-07 00:43:20 +0900

    ♻️ :: 학생 정보보기 서비스 구현

---
+++ src/main/java/com/example/cmdproject_team2/domain/user/service/commonService/GetStudentDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.cmdproject_team2.domain.user.service.commonService;
+
+import com.example.cmdproject_team2.domain.user.domain.User;
+import com.example.cmdproject_team2.domain.user.domain.UserRepository;
+import com.example.cmdproject_team2.domain.user.presentation.dto.response.StudentDetailsResponse;
+import com.example.cmdproject_team2.global.exception.user.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class GetStudentDetailsService {
+
+    private final UserRepository userRepository;
+
+    public StudentDetailsResponse getStudentDetails(String userId) {
+
+        User user = userRepository.findByUserId(userId)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+        return new StudentDetailsResponse(user);
+    }
+}
