c4aa92a184b68179c072b204d3fd37d73e938449
4mjeo <4mj.eo@dsm.hs.kr>
2023-08-07 15:01:39 +0900

    ♻️ :: 예외처리 추가

--- src/main/java/com/example/cmdproject_team2/domain/user/service/studentService/StudentSignUpService.java
+++ src/main/java/com/example/cmdproject_team2/domain/user/service/studentService/StudentSignUpService.java
@@ -5,6 +5,7 @@ import com.example.cmdproject_team2.domain.user.domain.User;
 import com.example.cmdproject_team2.domain.user.domain.UserRepository;
 import com.example.cmdproject_team2.domain.user.enums.UserType;
 import com.example.cmdproject_team2.domain.user.presentation.dto.request.StudentSignupRequest;
+import com.example.cmdproject_team2.global.exception.user.UserExistsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,7 +24,11 @@ public class StudentSignUpService {
 
         String password = passwordEncoder.encode(request.getPassword());
 
-        duplicateUserIdService.execute(request.getUserId());
+        try {
+            duplicateUserIdService.execute(request.getUserId());
+        } catch (UserExistsException e) {
+            throw new RuntimeException("중복된 아이디가 존재합니다.");
+        }
 
         User user = User.builder()
                 .userId(request.getUserId())
