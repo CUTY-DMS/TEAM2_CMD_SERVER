45b3409f1079ee8019401dca1e5f68c2dc333cf8
4mjeo <4mj.eo@dsm.hs.kr>
2023-08-07 20:35:01 +0900

    ♻️ :: 비번찾기 request 생성

---
+++ src/main/java/com/example/cmdproject_team2/domain/user/presentation/dto/request/FindPasswordRequest.java
@@ -0,0 +1,13 @@
+package com.example.cmdproject_team2.domain.user.presentation.dto.request;
+
+public class FindPasswordRequest {
+    private String userEmail;
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+}
