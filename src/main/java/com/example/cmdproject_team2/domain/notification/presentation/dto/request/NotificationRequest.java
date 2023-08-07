c12bbd6171ccd7c7dd92260ffd9c77199fd6c28e
JungHyun <b57062562@gmail.com>
2023-08-07 11:45:59 +0900

    ♻️ :: NotNull 어노테이션 추가

--- src/main/java/com/example/cmdproject_team2/domain/notification/presentation/dto/request/NotificationRequest.java
+++ src/main/java/com/example/cmdproject_team2/domain/notification/presentation/dto/request/NotificationRequest.java
@@ -4,15 +4,20 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NotificationRequest {
 
+    @NotNull
     private String title;
 
+    @NotNull
     private String content;
 
+    @NotNull
     private LocalDateTime expiredAt;
+
 }
