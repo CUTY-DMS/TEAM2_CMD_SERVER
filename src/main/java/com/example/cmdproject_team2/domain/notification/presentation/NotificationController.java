77b7d5e7808ebc1084a6c9a48263ec48641458ad
JungHyun <b57062562@gmail.com>
2023-08-07 11:46:09 +0900

    ♻️ :: 줄바꿈 정리

--- src/main/java/com/example/cmdproject_team2/domain/notification/presentation/NotificationController.java
+++ src/main/java/com/example/cmdproject_team2/domain/notification/presentation/NotificationController.java
@@ -10,6 +10,7 @@ import com.example.cmdproject_team2.domain.notification.service.publicService.Ge
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -20,33 +21,33 @@ public class NotificationController {
     private final GetNotificationDetailsService getNotificationDetailsService;
     private final GetNotificationListService getNotificationListService;
 
-
     //ADMIN
     private final WriteNotificationService writeNotificationService;
     private final ModifyNotificationService modifyNotificationService;
     private final DeleteNotificationService deleteNotificationService;
-
 
     //PUBLIC
     @GetMapping("/notification/{notificationId}")
     public NotificationResponse getNotificationDetails(@PathVariable long notificationId) {
         return getNotificationDetailsService.getNotificationDetails(notificationId);
     }
+    
     @GetMapping("/notification/list")
     public List<NotificationResponse> getNotificationList() {
         return getNotificationListService.getNotificationList();
     }
 
-
     //ADMIN
     @PostMapping("/admin/notification")
-    public void writeNotification(@RequestBody NotificationRequest request) {
+    public void writeNotification(@RequestBody @Valid NotificationRequest request) {
         writeNotificationService.writeNotification(request);
     }
+
     @PatchMapping("/admin/notification/{notificationId}")
     public void modifyNotification(@PathVariable long notificationId, @RequestBody NotificationRequest request) {
         modifyNotificationService.modifyNotification(notificationId, request);
     }
+
     @DeleteMapping("/admin/notification/{notificationId}")
     public void deleteNotification(@PathVariable long notificationId) {
         deleteNotificationService.deleteNotification(notificationId);
