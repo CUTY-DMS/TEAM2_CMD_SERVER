80c65b810292d278bd3e855121dad7fb303ba7d6
4mjeo <4mj.eo@dsm.hs.kr>
2023-08-07 20:44:14 +0900

    ♻️ ::어노테이션 추가

--- src/main/java/com/example/cmdproject_team2/domain/user/presentation/dto/request/FindPasswordRequest.java
+++ src/main/java/com/example/cmdproject_team2/domain/user/presentation/dto/request/FindPasswordRequest.java
@@ -1,13 +1,13 @@
 package com.example.cmdproject_team2.domain.user.presentation.dto.request;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class FindPasswordRequest {
+
     private String userEmail;
-
-    public String getUserEmail() {
-        return userEmail;
-    }
-
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
 }
