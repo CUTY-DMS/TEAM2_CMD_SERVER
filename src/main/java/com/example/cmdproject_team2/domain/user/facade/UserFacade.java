1338e416367ccf22c90595eded026160ddd9da7e
4mjeo <4mj.eo@dsm.hs.kr>
2023-08-07 15:37:31 +0900

    ♻️ :: currentUser 예외추가

--- src/main/java/com/example/cmdproject_team2/domain/user/facade/UserFacade.java
+++ src/main/java/com/example/cmdproject_team2/domain/user/facade/UserFacade.java
@@ -3,9 +3,11 @@ package com.example.cmdproject_team2.domain.user.facade;
 import com.example.cmdproject_team2.domain.user.domain.User;
 import com.example.cmdproject_team2.domain.user.domain.UserRepository;
 import com.example.cmdproject_team2.global.exception.user.EmailMatchedException;
+import com.example.cmdproject_team2.global.exception.user.NotAuthenticatedException;
 import com.example.cmdproject_team2.global.exception.user.UserExistsException;
 import com.example.cmdproject_team2.global.exception.user.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -16,10 +18,16 @@ public class UserFacade {
     private final UserRepository userRepository;
 
     public User currentUser() {
-        String userId = SecurityContextHolder.getContext().getAuthentication().getName();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new NotAuthenticatedException("인증되지 않는 사용자입니다.");
+        }
+
+        String userId = authentication.getName();
 
         return userRepository.findByUserId(userId)
-                .orElseThrow(()->UserNotFoundException.EXCEPTION);
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
     }
 
     public void checkUserExist(String userId) {
